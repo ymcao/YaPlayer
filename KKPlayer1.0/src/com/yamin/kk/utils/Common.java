80166c3c0ab541a509d22b236be85a035873d91b
曹亚民 <caoyamin@192.168.7.245>
2017-01-09 22:23:44 +0800

    Refactor
    Change to android  studio

--- KKPlayer1.0/src/com/yamin/kk/utils/Common.java
+++
@@ -1,8 +0,0 @@
-package com.yamin.kk.utils;
-
-public class Common {
-	public final static String NAME = "KKYAminSharedPreferences";
-	public final static String VIDEO_RESUME_TIME = "VideoResumeTime";
-	public final static String VIDEO_SUBTITLE_FILES = "VideoSubtitleFiles";
-
-}
--- KKPlayer1.0/src/com/yamin/kk/view/OnScrollChangedListener.java
+++
@@ -1,5 +0,0 @@
-package com.yamin.kk.view;
-
-public abstract interface OnScrollChangedListener {
-	public abstract void onScrollChanged(int top, int oldTop);
-}
